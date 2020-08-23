9239ca7bfa973359bc6798190b8f343821f9473e
chendj <1660319728@qq.com>
2020-08-23 10:13:04 +0000

    1.5

--- juc_study/src/com/study/Main.java
+++ juc_study/src/com/study/Main.java
@@ -2,7 +2,27 @@ package com.study;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("child thread is in sleep");
+                    Thread.sleep(10000);
+                    System.out.println("child thread is in awake");
+                } catch (InterruptedException e) {
+                    System.out.println(Thread.currentThread().getName());
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.setName("test");
+        thread.start();
+
+        Thread.sleep(2000);
+
+        thread.interrupt();
     }
 }
---
+++ juc_study/src/com/study/SleepTest2.java
@@ -0,0 +1,46 @@
+package com.study;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SleepTest2 {
+    private static final Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    System.out.println("child threadA is in sleep");
+                    Thread.sleep(10000);
+                    System.out.println("child threadA is in awake");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    System.out.println("child threadB is in sleep");
+                    Thread.sleep(10000);
+                    System.out.println("child threadB is in awake");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+    }
+}
