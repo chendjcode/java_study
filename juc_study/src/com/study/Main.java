a70db79af07ca0df0eb81bf69c32986e0482518c
chendj <1660319728@qq.com>
2020-08-24 10:11:07 +0000

    1.8

--- juc_study/src/com/study/Main.java
+++ juc_study/src/com/study/Main.java
@@ -4,25 +4,19 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = new Thread(new Runnable() {
+        Thread threadOne = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    System.out.println("child thread is in sleep");
-                    Thread.sleep(10000);
-                    System.out.println("child thread is in awake");
-                } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName());
-                    e.printStackTrace();
-                }
+                while (!Thread.currentThread().interrupted()) { }
+                System.out.println("threadOne isInterrupted:" + Thread.currentThread().isInterrupted());
             }
         });
 
-        thread.setName("test");
-        thread.start();
+        threadOne.start();
 
-        Thread.sleep(2000);
+        threadOne.interrupt();
 
-        thread.interrupt();
+        threadOne.join();
+        System.out.println("main thread is over");
     }
 }
---
+++ juc_study/src/com/study/Test.java
@@ -0,0 +1,8 @@
+package com.study;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "hello world";
+        System.out.println(str.substring(0, 2));
+    }
+}
